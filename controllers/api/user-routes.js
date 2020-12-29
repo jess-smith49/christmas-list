d6f5b0626dc60ecdc7ef4a05456fc8c80357d5b8
jess-smith49 <js.jessicasmith774@gmail.com>
2020-12-28 19:37:25 -0600

    user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,6 +60,29 @@ router.get('/:username', (req, res) => {
     })
 });
 
+//SIGN UP
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
 
 //LOGIN
 router.post('/login', (req, res) => {
