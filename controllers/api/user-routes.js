69a87d6be9d76f8cec6c46be728de11a33eb172c
jess-smith49 <js.jessicasmith774@gmail.com>
2020-12-28 19:14:57 -0600

    login and logout code added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const {User} = require('../../models');
+const { route } = require('./list-routes');
 
 
 //FIND ALL USERS
@@ -58,5 +59,48 @@ router.get('/:username', (req, res) => {
         res.status(500).json(err)
     })
 });
+
+
+//LOGIN
+router.post('/login', (req, res) => {
+    console.log(req.body, "===============");
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that username found.'});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in.'});
+        });
+    });
+});
+
+//LOGOUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+
 
 module.exports = router;
